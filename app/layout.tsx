219fdb20e9a3cb5cf20fdc502c5e8b3d6cf935b0
CeeBott <111609495+CeeBott@users.noreply.github.com>
2024-09-23 10:54:49 -0600

    Add meta description and title for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,28 +2,28 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Calorie Calculator',
-  description: 'Calculate your daily calorie needs with our easy-to-use tool.',
+  title: 'Calorie Compass: Personalized Calorie Calculator',
+  description: 'Discover your optimal daily calorie intake with Calorie Compass. Our precise calculator considers your age, gender, height, weight, and activity level for personalized results. Start your health journey today!',
   viewport: 'width=device-width, initial-scale=1',
   icons: {
     icon: '/CalorieCompassFavicon.svg',
   },
   openGraph: {
-    title: 'Calorie Calculator',
-    description: 'Calculate your daily calorie needs with our easy-to-use tool.',
+    title: 'Calorie Compass: Personalized Calorie Calculator',
+    description: 'Discover your optimal daily calorie intake with Calorie Compass. Our precise calculator considers your age, gender, height, weight, and activity level for personalized results. Start your health journey today!',
     images: [
       {
         url: '/Calorie-Calculator-Web-Preview.png',
         width: 1200,
         height: 630,
-        alt: 'Calorie Compass Calorie Calculator Web Preview',
+        alt: 'Calorie Compass Calorie Calculator',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Calorie Calculator',
-    description: 'Calculate your daily calorie needs with our easy-to-use tool.',
+    title: 'Calorie Compass: Personalized Calorie Calculator',
+  description: 'Discover your optimal daily calorie intake with Calorie Compass. Our precise calculator considers your age, gender, height, weight, and activity level for personalized results. Start your health journey today!',
     images: ['/Calorie-Calculator-Web-Preview.png'],
   },
 }
